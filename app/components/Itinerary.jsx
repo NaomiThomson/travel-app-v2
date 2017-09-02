98e7035d13c205f3b26edf7b3d73b3e18c7550e2
Naomi Thomson <naomithomson94@gmail.com>
2017-09-02 12:01:15 -0700

    delete itineraries

--- app/components/Itinerary.jsx
+++ app/components/Itinerary.jsx
@@ -5,15 +5,35 @@ var axios = require('axios');
 var actions = require('actions');
 
 var Itinerary = React.createClass({
+  onClick: function () {
+
+    let { currentItinerary, sessionInfo } = this.props;
+
+    let headerConfig = {
+      headers: { 'x-auth': sessionInfo.token }
+    };
+
+    axios.delete(`https://powerful-cliffs-81990.herokuapp.com/itinerary/${currentItinerary._id}`, headerConfig)
+    .then((res) => {
+      console.log(res)
+      this.props.history.push('/itineraries')
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+
+  },
 
   render: function () {
     let { currentItinerary } = this.props;
-    
+
     return (
       <div>
-        {currentItinerary.location}
-        {currentItinerary.startDate}
-        {currentItinerary.endDate}
+        {currentItinerary.location}<br/>
+        {currentItinerary.startDate}<br/>
+        {currentItinerary.endDate}<br/>
+
+        <button onClick={this.onClick} classLocation="btn" type="submit">Delete</button>
       </div>
     )
   }
